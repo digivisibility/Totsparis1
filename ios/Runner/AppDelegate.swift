8e1801fa4222159bd1e52a877c91d0934d97a413
digivisibility <139612974+digivisibility@users.noreply.github.com>
2026-01-08 10:28:08 +0530

    Add Firebase integration in AppDelegate
    Integrate Firebase into the iOS app by importing the Firebase module and configuring it during app launch.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import Firebase // 1. Add this line
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    FirebaseApp.configure() // 2. Add this line BEFORE the registrant
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
