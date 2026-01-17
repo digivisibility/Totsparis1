a12cb0e2b37d7f9d89f9d0e487f0606d044b437f
Shashi <digivisibility@gmail.com>
2026-01-17 12:49:23 +0530

    app crash1

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,92 @@
-import 'package:community_material_icon/community_material_icon.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_stripe/flutter_stripe.dart';
-import 'package:iconly/iconly.dart';
-import 'package:maanstore/screens/Theme/theme.dart';
-import 'package:maanstore/screens/home_screens/home.dart';
-import 'package:maanstore/screens/splash_screen/splash_screen_one.dart';
-import 'package:onesignal_flutter/onesignal_flutter.dart';
-import 'package:provider/provider.dart' as pro;
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-import 'Providers/language_change_provider.dart';
-import 'const/constants.dart';
-import 'generated/l10n.dart';
-
-Future<void> main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize Firebase
-  await Firebase.initializeApp();
   
-  // Setup Crashlytics
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-  PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
-
-  // Initialize Theme
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-  final String savedTheme = prefs.getString('theme') ?? 'light';
-  _themeManager.toggleTheme(savedTheme == 'dark');
-
   // Set Orientation
-  await SystemChrome.setPreferredOrientations([
+  SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
 
-  // Initialize OneSignal
-  OneSignal.Debug.setLogLevel(OSLogLevel.none);
-  OneSignal.initialize(oneSignalAppId);
-  OneSignal.Notifications.requestPermission(true);
-
-  // Initialize Stripe
-  Stripe.publishableKey = stripePublishableKey;
-
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(const MyApp());
 }
 
-ThemeManager _themeManager = ThemeManager();
-
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tots Paris',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const MainWebView(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class MainWebView extends StatefulWidget {
+  const MainWebView({super.key});
+
   @override
-  void dispose() {
-    _themeManager.removeListener(themeListener);
-    super.dispose();
-  }
+  State<MainWebView> createState() => _MainWebViewState();
+}
+
+class _MainWebViewState extends State<MainWebView> {
+  late final WebViewController _controller;
+  bool _isLoading = true;
 
   @override
   void initState() {
-    _themeManager.addListener(themeListener);
     super.initState();
-  }
-
-  themeListener() {
-    if (mounted) {
-      setState(() {});
-    }
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // You can update a progress bar here
+          },
+          onPageStarted: (String url) {
+            setState(() {
+              _isLoading = true;
+            });
+          },
+          onPageFinished: (String url) {
+            setState(() {
+              _isLoading = false;
+            });
+          },
+          onWebResourceError: (WebResourceError error) {
+            debugPrint('WebView error: ${error.description}');
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://totsparis.com/'));
   }
 
   @override
   Widget build(BuildContext context) {
-    return pro.ChangeNotifierProvider<LanguageChangeProvider>(
-      create: (context) => LanguageChangeProvider(),
-      child: Builder(
-        builder: (context) => MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: lightTheme,
-          darkTheme: darkTheme,
-          themeMode: _themeManager.themeMode,
-          locale: pro.Provider.of<LanguageChangeProvider>(context, listen: true).currentLocale,
-          localizationsDelegates: const [
-            S.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            WebViewWidget(controller: _controller),
+            if (_isLoading)
+              const Center(
+                child: CircularProgressIndicator(),
+              ),
           ],
-          home: const SplashScreenOne(),
-          supportedLocales: S.delegate.supportedLocales,
-          builder: EasyLoading.init(),
         ),
       ),
     );
